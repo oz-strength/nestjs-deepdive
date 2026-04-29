b307014196c70741cc454161be59f4b786de8ad7
seungwoo <sw6016@hanmail.net>
2026-04-29 18:59:56 +0900

    #1 ND-1 Decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,31 @@
-import { Controller, Request, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/common/decorator/public.decorator';
+import { ApiPostResponse } from 'src/common/decorator/swagger.decorator';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { SigninReqDto, SignupReqDto } from './dto/req.dto';
+import { SigninResDto, SignupResDto } from './dto/res.dto';
 
 @ApiTags('Auth')
+@ApiExtraModels(SignupResDto, SigninResDto)
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiPostResponse(SignupResDto)
+  @Public()
   @Post('signup')
-  async signup() {
-    return this.authService.signup('email', 'password');
+  async signup(@Body() { email, password, passwordConfirm }: SignupReqDto) {
+    if (password !== passwordConfirm) throw new BadRequestException();
+
+    const { id } = await this.authService.signup(email, password);
+    return { id };
   }
 
+  @ApiPostResponse(SigninResDto)
+  @Public()
   @Post('signin')
-  async signin(@Request() req) {
-    return this.authService.signin(req.user);
+  async signin(@Body() { email, password }: SigninReqDto) {
+    return this.authService.signin(email, password);
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,31 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { ApiGetItemsResponse, ApiGetResponse } from 'src/common/decorator/swagger.decorator';
+import { User, UserAfterAuth } from 'src/common/decorator/user.decorator';
+import { PageReqDto } from 'src/common/dto/req.dto';
+import { PageResDto } from 'src/common/dto/res.dto';
+import { FindUserReqDto } from './dto/req.dto';
+import { FindUserResDto } from './dto/res.dto';
 import { UserService } from './user.service';
 
 @ApiTags('User')
+@ApiExtraModels(FindUserReqDto, FindUserResDto, PageResDto)
 @Controller('api/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBearerAuth()
+  @ApiGetItemsResponse(FindUserResDto)
   @Get()
-  findAll() {
+  findAll(@Query() { page, size }: PageReqDto, @User() user: UserAfterAuth) {
+    console.log(user);
     return this.userService.findAll();
   }
 
+  @ApiBearerAuth()
+  @ApiGetResponse(FindUserResDto)
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') { id }: FindUserReqDto) {
     return this.userService.findOne(id);
   }
 }
